cf6d92e5dc32dc37e37e2cdcf85c02edc73269b8
cvija <cvijanperanovic@aol.com>
2019-10-22 12:46:40 +0200

    Dodat kostur projekta

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//-----------------------------------------------
+//Nedelja 1
+//Nedelja 1, Dan 1
+/* Danas smo presli osnove osnova HTML-a, CSS-a, JS-a;
+Sledeci kod nam ispisuje poruku kao i u web stranicu ubacuje neki element
+*/
+console.log('Hello by js');
+console.log('Hello World!');
+
+document.getElementById('app').innerHTML='<p>Hello to BootcampPizza from JavaScript!</p>';

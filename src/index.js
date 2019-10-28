d016a4e61b25097aa7eda4f3992367c3a04e6992
cvija <cvijanperanovic@aol.com>
2019-10-28 13:52:18 +0100

    added stuff

--- src/index.js
+++ src/index.js
@@ -12,12 +12,20 @@ console.log(app);
 
 app.innerHTML = '<p>Hello to BootcampPizza from JavaScript!</p>';
 
+const  facebook_url='https://www.facebook.com';
+const  youtube_url='https://www.youtube.com';
+
+var facebook_ico_path='assets/images/fb_ico.png';
+var youtube_ico_path='assets/images/yt_ico.png';
+
 const social = document.getElementById('social');
 social.innerHTML =
-    '<a href="https://www.facebook.com" target="_blank"><img class="social-icon" src="assets/images/fb_ico.png" /></a>';
+    `<a href="${facebook_url}" target="_blank"><img class="social-icon" src="${facebook_ico_path}" /></a>`;
 
 
-social.innerHTML += '<a href="https://www.youtube.com" target="_blank"><img class="social-icon" src="assets/images/yt_ico.png" /></a>';
+social.innerHTML += `<a href="${youtube_url}" target="_blank"><img class="social-icon" src="${youtube_ico_path}" /></a>`;
+
+
 
 //Dodati ikonice za tviter i instagram
 
@@ -122,3 +130,81 @@ console.log(c);
     //let ab*;
     //let *ab;
 }*/
+
+var name = 'Bootcamp Pizza';
+var street = 'Bulevar kralja Aleksandra';
+var streetNumber = 73;
+var postalCode = 11000;
+var city = 'Belgrade';
+var country = 'Republic of Serbia';
+var phoneNumber = '+381(0)64-12-34-567';
+
+const storeNameContainer = document.getElementById('store-name');
+storeNameContainer.innerHTML = name;
+
+const storeAddressContainer = document.getElementById('store-address');
+storeAddressContainer.innerHTML = `<p>${street} ${streetNumber}, ${postalCode} ${city}<br/>${country}</p>Phone: ${phoneNumber}`;
+
+var radius = 16;
+//var pizzaSurface=radius*radius*3.14;
+//var pizzaSurface=radius*radius*Math.PI;
+var pizzaSurface = Math.pow(radius, 2) * Math.PI;
+console.log(`Pizza surface is ${Math.round(pizzaSurface)} cm²`);
+//console.log('Pizza surface is '+Math.round(pizzaSurface)+' cm²');
+
+//Given the sliceNumber and radius, calculate the slice length
+
+//Zadatak 3
+/*
+{
+    let depoMalih = 10;
+    let depoVelikih = 20;
+    {
+        let kolMalih = 3;
+        let kolvelikih = 2;
+        let ukupnoKauc = kolMalih * depoMalih + kolvelikih * depoVelikih;
+        console.log('Ukupno za kauciju: ' + ukupnoKauc);
+    }
+}
+*/
+
+//---------------------------------------------------------
+//Date and Time
+
+//PAZNJA - KORISTICEMO KASNIJE
+{
+    let dateTimeNow = new Date();
+    console.log(dateTimeNow);
+
+    let hours = dateTimeNow.getHours();
+    let minutes = dateTimeNow.getMinutes();
+    let seconds = dateTimeNow.getSeconds();
+    let timeStampString = `${hours}:${minutes}:${seconds}`;
+    console.log(timeStampString);
+    //Za danasnji datum
+    console.log(dateTimeNow.getDay());
+
+    let totalSeconds=hours*3600+minutes*60+seconds; 
+    //let totalSeconds=(hours*60+minutes)*60+seconds;
+
+    console.log(`Total number of seconds today is ${totalSeconds}`);
+    //Postavicemo datum na neki dan koji je nedelja
+    //dateTimeNow.setDate(27);
+    //console.log(dateTimeNow.getDay());
+
+    //Write date and time in the following format: [{date} - time]
+
+    let dayInMonth = dateTimeNow.getDate();
+    let month = dateTimeNow.getMonth() + 1;
+    let year = dateTimeNow.getFullYear();
+    let dateStampString = `${dayInMonth}-${month}-${year}`;
+    console.log(dateStampString);
+
+    let dateTimeStampString = `[{${dateStampString}} - ${timeStampString}]`;
+    console.log(dateTimeStampString);
+    
+
+
+
+}
+
